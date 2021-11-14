a1d50cf086318e243448aa1b4545c4c57159fc02
Thang Long VU <vuphanthanglong@gmail.com>
2021-11-14 04:56:24 +0100

    Revert "Fixing some possible bugs in transaction.cpp. 1. Initialization of dummy variable i in for loop. 2. Some extra ;."
    This reverts commit 2f36a875

---
+++ cmake-build-debug/CMakeFiles/xcoin.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xcoin.dir/XBlockchain/Blockchain.cpp.obj"
+  "CMakeFiles/xcoin.dir/XBlockchain/bignumber.cpp.obj"
+  "CMakeFiles/xcoin.dir/XBlockchain/block.cpp.obj"
+  "CMakeFiles/xcoin.dir/XBlockchain/sha256.cpp.obj"
+  "CMakeFiles/xcoin.dir/main.cpp.obj"
+  "xcoin.exe"
+  "xcoin.exe.manifest"
+  "xcoin.lib"
+  "xcoin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xcoin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
