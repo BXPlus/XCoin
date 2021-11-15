b43a83035deb30c6774dc223d783cc8d39d5d086
Malo-Lahogue <malo.lahogue@gmail.com>
2021-11-15 15:31:29 +0100

    Secon update of the class after having found a conversion to exadecimal of the keys

--- src/XNode/keys.cpp
+++ src/XNode/keys.cpp
@@ -4,10 +4,7 @@
 //for key generation, we will use the ECDSA which is also used by bitcoin btw.
 //It offers a more secure mechanism than RSA
 
-
 #include "keys.h"
-
-
 
 Keys::Keys(){
 
@@ -22,18 +19,13 @@ Keys::Keys(){
         std::cout << " key generation problem" << std::endl;
     }
 
-
-
     // Set up private key in prv
     prv = EC_KEY_get0_private_key(key);
-    //EC_KEY_set_private_key(key, prv);
     if(!prv){
         std::cout << "Error getting private key" << std::endl;
     }
-    char *priv_key = BN_bn2hex(prv);
-
-    std::cout << prv << "      " << priv_key << std::endl;
-
+    //we get the hex form
+    priv_key = BN_bn2hex(prv);
 
     // Set up public key in pub
     pub = EC_KEY_get0_public_key(key);
@@ -41,32 +33,14 @@ Keys::Keys(){
     if(!pub){
         std::cout << "Error getting public key" << std::endl;
     }
+    //we set the hex form
     EC_KEY *eckey = NULL;
     const EC_GROUP *group = NULL;
     eckey = EC_KEY_new_by_curve_name(NID_secp256k1);
     group = EC_KEY_get0_group(eckey);
     BN_CTX *ctx;
     ctx = BN_CTX_new(); // ctx is an optional buffer to save time from allocating and deallocating memory whenever required
-    char *pub_key = EC_POINT_point2hex(group, pub, POINT_CONVERSION_UNCOMPRESSED, ctx);
+    pub_key = EC_POINT_point2hex(group, pub, POINT_CONVERSION_UNCOMPRESSED, ctx);
 
-    std::cout << pub << "      " << pub_key << std::endl;
-
-    /*
-    BIGNUM *priv_key;
-    EC_GROUP *group = EC_GROUP_new_by_curve_name(NID_secp256k1);
-    EC_KEY *key = EC_KEY_new();
-    EC_POINT *pub_key = EC_POINT_new(group);
-
-    EC_KEY_set_group(key, group);
-    EC_KEY_set_private_key(key, priv_key);
-    EC_POINT_mul(group, pub_key, priv_key, NULL, NULL, NULL);
-    EC_KEY_set_public_key(key, pub_key);
-
-    prv = priv_key;
-    pub = pub_key;
-
-    std::cout << priv_key << std::endl;
-*/
-
-    //EC_KEY_free(key);
+    EC_KEY_free(key); //we free the memory occupied by the useless 'key'
 }
--- src/XNode/keys.h
+++ src/XNode/keys.h
@@ -5,7 +5,6 @@
 #ifndef XCOIN_KEYS_H
 #define XCOIN_KEYS_H
 #include <iostream>
-
 #include <openssl/obj_mac.h>
 #include <openssl/ec.h> //the elliptic curve library from openSSL
 
@@ -14,7 +13,10 @@ public:
     Keys(); //constructor which creates the private and public key
 
     BIGNUM const* prv;        //private key (which is a big integer)
+    char const* priv_key;         //private key in hexadecimal form
     EC_POINT const* pub;      //public key which is a point made of two coordinates and depending on the private key
+    char const* pub_key;         //public key in hexadecimal form
+
 
 };
 
