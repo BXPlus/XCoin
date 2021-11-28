bd401d3626aab90fec7c53d57837ec1b033be918
arthur-failler <arthurfailler@gmail.com>
2021-11-28 22:29:20 +0100

    design balance

--- build-ui.pro-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/ui_balancewidget.h
+++ build-ui.pro-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/ui_balancewidget.h
@@ -11,7 +11,6 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QLabel>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -19,8 +18,6 @@ QT_BEGIN_NAMESPACE
 class Ui_BalanceWidget
 {
 public:
-    QLabel *label;
-    QLabel *label_2;
 
     void setupUi(QWidget *BalanceWidget)
     {
@@ -30,17 +27,6 @@ public:
         QFont font;
         font.setFamily(QString::fromUtf8("Arial"));
         BalanceWidget->setFont(font);
-        label = new QLabel(BalanceWidget);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(270, 60, 351, 71));
-        label_2 = new QLabel(BalanceWidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(340, 130, 201, 41));
-        QFont font1;
-        font1.setFamily(QString::fromUtf8("Arial Black"));
-        font1.setBold(true);
-        font1.setWeight(75);
-        label_2->setFont(font1);
 
         retranslateUi(BalanceWidget);
 
@@ -50,8 +36,6 @@ public:
     void retranslateUi(QWidget *BalanceWidget)
     {
         BalanceWidget->setWindowTitle(QApplication::translate("BalanceWidget", "Form", nullptr));
-        label->setText(QApplication::translate("BalanceWidget", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">Your current Balance is :</span></p></body></html>", nullptr));
-        label_2->setText(QApplication::translate("BalanceWidget", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">21,02 XCoin</span></p></body></html>", nullptr));
     } // retranslateUi
 
 };
--- ui/balancewidget.cpp
+++ ui/balancewidget.cpp
@@ -2,51 +2,113 @@
 #include "ui_balancewidget.h"
 #include <custombutton.h>
 #include <QBoxLayout>
+#include <QLabel>
+#include <QSizePolicy>
+#include <QStaticText>
+#include <QSize>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QGraphicsWidget>
+
 
 BalanceWidget::BalanceWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::BalanceWidget)
 {
     ui->setupUi(this);
-    setWindowTitle("balance");
-    setMinimumSize(500,600);
+
+    QVBoxLayout* vbalanceLayout = new QVBoxLayout(this);
+
+    //Upper Box Name
+    QLabel* labelbalance = new QLabel(this);
+    vbalanceLayout->addWidget(labelbalance);
+    labelbalance->setObjectName("labelbalance");
+    labelbalance->setText(QString("Your Current Balance is : "));
+    labelbalance->setMinimumSize(400, 45);
+    labelbalance->setAlignment(Qt::AlignHCenter);
+
+    //Lower Box Name
+    QLabel* latest_transac_title = new QLabel(this);
+    vbalanceLayout->addWidget(latest_transac_title);
+    latest_transac_title->setObjectName("latesttransac");
+    latest_transac_title->setText(QString("History of Transactions : "));
+    latest_transac_title->setMinimumSize(400, 45);
+    latest_transac_title->setAlignment(Qt::AlignHCenter);
+
+    //Scroll Bar
+
+    QWidget* scrollWidget = new QWidget(this);
+    QScrollArea* scroll = new QScrollArea (scrollWidget) ;
+    QBoxLayout* boxWidgetLayout = new QBoxLayout(QBoxLayout::TopToBottom, scrollWidget) ;
+
+    //Remove horizontal scrollbar
+    scroll->horizontalScrollBar()->setEnabled(false);
+    scroll->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    boxWidgetLayout->addWidget(scroll);
+    boxWidgetLayout->setAlignment(scroll,Qt::AlignHCenter);
+
+    QWidget* transacContainer = new QWidget(scroll);
+    QVBoxLayout* transacLayout = new QVBoxLayout(transacContainer);
+
+    scroll->setWidget(transacContainer);
+
+    transacContainer->resize(1000,500);
+    QWidget* box1 = new QWidget(transacContainer);
+    QWidget* box2 = new QWidget(transacContainer);
+    QWidget* box3 = new QWidget(transacContainer);
+    QWidget* box4 = new QWidget(transacContainer);
+    QWidget* box5 = new QWidget(transacContainer);
+    QWidget* box6 = new QWidget(transacContainer);
+
+    box1->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    box2->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    box3->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    box4->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    box5->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    box6->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+
+
+    transacLayout->addWidget(box1);
+    transacLayout->addWidget(box2);
+    transacLayout->addWidget(box3);
+    transacLayout->addWidget(box4);
+    transacLayout->addWidget(box5);
+    transacLayout->addWidget(box6);
+
+
+    vbalanceLayout->addWidget(scrollWidget);
+
+
+
+    //setWindowTitle("balance");
+    //setMinimumSize(500,600);
 
     //Set the layout for the balance widget
-    balanceWidget = new QWidget(this);
-    hbalanceLayout = new QHBoxLayout(balanceWidget);
+    //balanceWidget = new QWidget(this);
+    //hbalanceLayout = new QHBoxLayout(balanceWidget);
 
-    //test
-    //vbalanceLayout = new QVBoxLayout(balanceWidget);
-    //CustomButton* test = new CustomButton("wsh", balanceWidget);
-    //hbalanceLayout->addLayout(vbalanceLayout);
-    //hbalanceLayout->addWidget(test);
+
 
     //Set the layout for the transactions
-    transacContainer = new QWidget(balanceWidget);
-    transacLayout = new QVBoxLayout(transacContainer);
-    transacContainer->setLayout(hbalanceLayout);
-    hbalanceLayout->addWidget(transacContainer);
-    transacContainer->setMaximumWidth(200);
-    transacLayout->addSpacing(10);
+    //transacContainer = new QWidget(balanceWidget);
+    //transacLayout = new QVBoxLayout(transacContainer);
+    //hbalanceLayout->addWidget(transacContainer);
+    //transacContainer->setMaximumWidth(200);
+    //transacLayout->addSpacing(10)
+
+    //balanceWidget->setLayout(transacLayout);
 
 
-    //test
-    //hbalanceLayout->addWidget(test);
+    //for (int i=0; i<6; i++){
+      //  QLabel *label = new QLabel(transacContainer);
+      //  label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+      //  label->setText("first");
+      //  label->setAlignment(Qt::AlignBottom | Qt::AlignRight);
+      //  transacLayout->addWidget(label);
+    //}
 
-
-    balanceWidget->setLayout(transacLayout);
-
-    //test
-    //balanceWidget->setLayout(hbalanceLayout);
-
-    for (int i=0; i<6; i++){
-        CustomButton* btn = new CustomButton("user i", transacContainer);
-        btn->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
-        btn->setCursor(Qt::PointingHandCursor);
-        transacLayout->addWidget(btn);
-    }
-
-    setLayout(transacLayout);
+    //setLayout(transacLayout);
 }
 
 BalanceWidget::~BalanceWidget()
