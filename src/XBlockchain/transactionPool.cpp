d42e9f026f179aa681032b4b318d012a08004b0a
youssef-chaabouni <youssef.chaabouni@polytechnique.edu>
2021-11-28 16:26:54 +0100

    Added header file for transactionPool and fixed some errors

--- src/XBlockchain/transactionPool.cpp
+++ src/XBlockchain/transactionPool.cpp
@@ -1,22 +1,53 @@
-#include "transaction.h"
-#include "wallet.cpp"
+//
+// Created by youssef on 11/28/21.
+//
+
+#include "transactionPool.h"
 #include <algorithm>
 
-vector<Transaction> transactionPool;
+std::vector<Transaction> transactionPool;
 
-vector<Transaction> getTransactionPool() {
+std::vector<Transaction> getTransactionPool() {
     return transactionPool;
 }
 
 /*
-Transaction sendTransaction(string address, int amount) {
+Transaction sendTransaction(std::string address, int amount) {
     Transaction tx = createTransaction(address, amount, getPrivateFromWallet(), getUnspentTxOuts(), getTransactionPool());
     addToTransactionPool(tx, getUnspentTxOuts());
     return tx;
 }
 */
 
-void addToTransactionPool(Transaction tx, vector<UnspentTxOut> unspentTxOuts) {
+std::vector<TxIn> getTxPoolIns(std::vector<Transaction> aTransactionPool) {
+    std::vector<TxIn> res;
+    for (int i = 0; i < aTransactionPool.size(); i++){
+        std::vector<TxIn> tx_txIns = aTransactionPool[i].txIns;
+        res.insert(res.end(), tx_txIns.begin(), tx_txIns.end());
+    }
+    return res;
+}
+
+bool isValidTxForPool(Transaction tx, std::vector<Transaction> aTransactionPool)
+{
+    std::vector<TxIn> txPoolIns = getTxPoolIns(aTransactionPool);
+    for(int i = 0; i < (tx.txIns).size(); i++){
+        TxIn txIn = tx.txIns[i];
+        bool txPoolContainsTxIn = false;
+        for(int j = 0; j < txPoolIns.size(); j++){
+            if (txPoolIns[j].txOutIndex == txIn.txOutIndex && txPoolIns[j].txOutId == txIn.txOutId){
+                txPoolContainsTxIn = true;
+            }
+        }
+        if (txPoolContainsTxIn){
+            cout << "txIn already found in the txPool";
+            return false;
+        };
+    };
+    return true;
+}
+
+void addToTransactionPool(Transaction tx, std::vector<UnspentTxOut> unspentTxOuts) {
     if (!tx.validateTransaction(unspentTxOuts)) {
         cout << "Trying to add invalid tx to pool";
         return;
@@ -25,12 +56,12 @@ void addToTransactionPool(Transaction tx, vector<UnspentTxOut> unspentTxOuts) {
         cout << "Trying to add invalid tx to pool";
         return;
     }
-    cout << "Adding transaction to transactionPool" //Add details about the transaction by implementing JSON.stringify(tx)
+    cout << "Adding transaction to transactionPool"; //Add details about the transaction by implementing JSON.stringify(tx)
     transactionPool.push_back(tx);
     return;
 }
 
-bool hasTxIn(TxIn txIn, vector<UnspentTxOut> unspentTxOuts) {
+bool hasTxIn(TxIn txIn, std::vector<UnspentTxOut> unspentTxOuts) {
     for (int i = 0; i < int(unspentTxOuts.size()); i++) {
         UnspentTxOut uTxO = unspentTxOuts[i];
         if (uTxO.txOutId == txIn.txOutId && uTxO.txOutIndex == txIn.txOutIndex) {
@@ -40,9 +71,9 @@ bool hasTxIn(TxIn txIn, vector<UnspentTxOut> unspentTxOuts) {
     return false;
 }
 
-void updateTransactionPool(vector<UnspentTxOut> unspentTxOuts) {
-    vector<Transaction> invalidTxs;
-    vector<Transaction> newPool;
+void updateTransactionPool(std::vector<UnspentTxOut> unspentTxOuts) {
+    std::vector<Transaction> invalidTxs;
+    std::vector<Transaction> newPool;
     for (int i = 0; i < transactionPool.size(); i++) {
         Transaction tx = transactionPool[i];
         bool isValid = true;
@@ -63,33 +94,3 @@ void updateTransactionPool(vector<UnspentTxOut> unspentTxOuts) {
         transactionPool = newPool;
     }
 }
-
-vector<TxIn> getTxPoolIns(vector<Transaction> aTransactionPool) {
-    vector<TxIn> res;
-    for (int i = 0; i < aTransactionPool.size(); i++){
-        vector<TxIn> tx_txIns = aTransactionPool[i].txIns;
-        res.insert(res.end(), tx_txIns.begin(), tx_txIns.end());
-    }
-    return res;
-}
-
-bool isValidTxForPool(Transaction tx, vector<Transaction> aTransactionPool)
-{
-    vector<TxIn> txPoolIns = getTxPoolIns(aTransactionPool);
-    for(int i = 0; i < (tx.txIns).size(); i++){
-        txIn = txIns[i];
-        bool txPoolContainsTxIn = false;
-        for(int j = 0; j < txPoolIns.size(); j++){
-            if (txPoolIns[j].txOutIndex == txIn.txOutIndex && txPoolIns[j].txOutId == txIn.txOutId){
-                txPoolContainsTxIn = true;
-            }
-        }
-        if (txPoolContainsTxIn){
-            cout << "txIn already found in the txPool";
-            return false
-        };
-    };
-    return true;
-}
-
-
