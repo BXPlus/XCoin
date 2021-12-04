ad06fb3ce9687ec5d506a5770dc09ded6f8b98c8
Cyrus Pellet <cyrus135@hotmail.fr>
2021-12-04 17:49:33 +0100

    Attempt to fix DNS handshake

--- src/XNode/node.cpp
+++ src/XNode/node.cpp
@@ -31,8 +31,10 @@ void XNode::Node::RunNode(const std::vector<std::string>& dnsSeedPeers) {
                                  ::xcoin::interchange::PingHandshake *response) {
     spdlog::debug("Ping received from " + context->peer());
 
-    //if (this->peers.count(context->peer())==0)
-        //TODO : Add new peer here
+    if (this->peers.count(context->peer())==0){
+        std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(context->peer(), grpc::InsecureChannelCredentials());
+        this->peers[context->peer()] = XNodeClient(channel);
+    }
     response->set_data(request->data());
     return ::grpc::Status::OK;
 }
@@ -43,9 +45,11 @@ XNode::Node::DNSSyncPeerList(::grpc::ServerContext *context, const ::xcoin::inte
     for (const xcoin::interchange::DNSEntry& remotePeer : request->entries())
         handleIncomingPeerData(remotePeer, std::unique_ptr<xcoin::interchange::XNodeControl::Stub>());
     for (auto const& x : this->peers){
-        xcoin::interchange::DNSEntry *entry = response->add_entries();
-        entry->set_ipport(x.first);
-        entry->set_publickey(x.second.publicKey);
+        if (x.first != context->peer()){
+            xcoin::interchange::DNSEntry *entry = response->add_entries();
+            entry->set_ipport(x.first);
+            entry->set_publickey(x.second.publicKey);
+        }
     }
     return ::grpc::Status::OK;
 }
@@ -75,16 +79,18 @@ bool XNode::Node::AttemptPeerConnection(const std::string& peerAddress) {
         xcoin::interchange::DNSHandshake dnsRequest;
         xcoin::interchange::DNSHandshake dnsReply;
         for(auto it = peers.begin(); it != peers.end(); ++it){
-            xcoin::interchange::DNSEntry *entry = dnsRequest.add_entries();
-            entry->set_ipport(it->first);
-            entry->set_publickey(it->second.publicKey);
+            if (it->first != peerAddress){
+                xcoin::interchange::DNSEntry *entry = dnsRequest.add_entries();
+                entry->set_ipport(it->first);
+                entry->set_publickey(it->second.publicKey);
+            }
         }
         ::grpc::ClientContext dnsContext;
         ::grpc::Status dnsStatus = this->peers[peerAddress].controlStub->DNSSyncPeerList(&dnsContext, dnsRequest, &dnsReply);
+        spdlog::debug("Will attempt to sync DNS with " + peerAddress);
         if (dnsStatus.ok()){
-            //for(const xcoin::interchange::DNSEntry& remotePeer : dnsReply.entries())
-                //this->handleIncomingPeerData(remotePeer, this->peers[peerAddress].rpcStub,
-                 //                            context);
+            for(const xcoin::interchange::DNSEntry& remotePeer : dnsReply.entries())
+                this->handleIncomingPeerData(remotePeer, std::move(this->peers[peerAddress].controlStub));
 
         }else spdlog::warn("DNS peer sync with " + peerAddress + " failed");
         return true;
