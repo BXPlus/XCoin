70dff50ed9235262f6ad85f7bc5ef5c1f2a30bd5
arthur-failler <arthurfailler@gmail.com>
2021-12-05 18:24:36 +0100

    Design Modification with names...

--- ui/balancewidget.cpp
+++ ui/balancewidget.cpp
@@ -47,19 +47,76 @@ BalanceWidget::BalanceWidget(QWidget *parent) :
 
     boxWidgetLayout->addWidget(scroll);
     boxWidgetLayout->setAlignment(scroll,Qt::AlignHCenter);
+    //scroll->setStyleSheet("background-color: grey");
 
     QWidget* transacContainer = new QWidget(scroll);
     QVBoxLayout* transacLayout = new QVBoxLayout(transacContainer);
 
     scroll->setWidget(transacContainer);
 
-    transacContainer->resize(1000,500);
+    transacContainer->resize(500,500);
     QWidget* box1 = new QWidget(transacContainer);
     QWidget* box2 = new QWidget(transacContainer);
     QWidget* box3 = new QWidget(transacContainer);
     QWidget* box4 = new QWidget(transacContainer);
     QWidget* box5 = new QWidget(transacContainer);
     QWidget* box6 = new QWidget(transacContainer);
+
+
+    //Names and Money Transaction in Boxes
+    //
+
+    //Layout for each box
+    QVBoxLayout* box1layout = new QVBoxLayout(box1);
+    QVBoxLayout* box2layout = new QVBoxLayout(box2);
+    QVBoxLayout* box3layout = new QVBoxLayout(box3);
+    QVBoxLayout* box4layout = new QVBoxLayout(box4);
+    QVBoxLayout* box5layout = new QVBoxLayout(box5);
+    QVBoxLayout* box6layout = new QVBoxLayout(box6);
+
+    //Names and Money of each transaction
+    QLabel* namebox1 = new QLabel(box1);
+    QLabel* namebox2 = new QLabel(box2);
+    QLabel* namebox3 = new QLabel(box3);
+    QLabel* namebox4 = new QLabel(box4);
+    QLabel* namebox5 = new QLabel(box5);
+    QLabel* namebox6 = new QLabel(box6);
+
+    QLabel* moneybox1 = new QLabel(box1);
+    QLabel* moneybox2 = new QLabel(box2);
+    QLabel* moneybox3 = new QLabel(box3);
+    QLabel* moneybox4 = new QLabel(box4);
+    QLabel* moneybox5 = new QLabel(box5);
+    QLabel* moneybox6 = new QLabel(box6);
+
+
+    QStringList names = {"Thomas", "Jean-Seb", "Marius", "Tim", "Youssef", "Clara"};
+    QStringList money = {"-3.2 XCoin", "0.4 XCoin", "2 XCoin", "-0.2 XCoin", "0.3 XCoin", "1 XCoin"};
+    QList<QWidget*> boxes = {box1, box2, box3, box4, box5, box6};
+    QList<QLabel*> nameboxes  = {namebox1, namebox2, namebox3, namebox4, namebox5, namebox6};
+    QList<QLabel*> moneyboxes  = {moneybox1, moneybox2, moneybox3, moneybox4, moneybox5, moneybox6};
+    QList<QVBoxLayout*> layoutboxes = {box1layout, box2layout, box3layout, box4layout, box5layout, box6layout};
+
+    for (int i=0; i<6; i++){
+        layoutboxes[i]->addWidget(nameboxes[i]);
+        nameboxes[i]->setObjectName(names[i]);
+        nameboxes[i]->setText(QString(names[i]));
+        nameboxes[i]->setMinimumSize(400, 100);
+        nameboxes[i]->setAlignment(Qt::AlignHCenter);
+
+        layoutboxes[i]->addWidget(moneyboxes[i]);
+        moneyboxes[i]->setObjectName(money[i]);
+        moneyboxes[i]->setText(QString(money[i]));
+        moneyboxes[i]->setMinimumSize(400, 45);
+        moneyboxes[i]->setAlignment(Qt::AlignHCenter);
+
+        //Choose the color of the border of the boxes
+        boxes[i]->setStyleSheet("border : 2px solid grey");
+        nameboxes[i]->setStyleSheet("border: grey");
+        moneyboxes[i]->setStyleSheet("border: grey");
+    }
+
+    //End Names and Money in Boxes
 
     box1->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
     box2->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
@@ -79,36 +136,6 @@ BalanceWidget::BalanceWidget(QWidget *parent) :
 
     vbalanceLayout->addWidget(scrollWidget);
 
-
-
-    //setWindowTitle("balance");
-    //setMinimumSize(500,600);
-
-    //Set the layout for the balance widget
-    //balanceWidget = new QWidget(this);
-    //hbalanceLayout = new QHBoxLayout(balanceWidget);
-
-
-
-    //Set the layout for the transactions
-    //transacContainer = new QWidget(balanceWidget);
-    //transacLayout = new QVBoxLayout(transacContainer);
-    //hbalanceLayout->addWidget(transacContainer);
-    //transacContainer->setMaximumWidth(200);
-    //transacLayout->addSpacing(10)
-
-    //balanceWidget->setLayout(transacLayout);
-
-
-    //for (int i=0; i<6; i++){
-      //  QLabel *label = new QLabel(transacContainer);
-      //  label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-      //  label->setText("first");
-      //  label->setAlignment(Qt::AlignBottom | Qt::AlignRight);
-      //  transacLayout->addWidget(label);
-    //}
-
-    //setLayout(transacLayout);
 }
 
 BalanceWidget::~BalanceWidget()
