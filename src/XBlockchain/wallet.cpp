ac29d6b021ed33c021edfa2ecb99847c197d4207
youssef-chaabouni <youssef.chaabouni@polytechnique.edu>
2021-12-05 19:32:23 +0100

    Implemented createTxOuts(), toUnsignedTxIn(), toUnsignedTxInArray() and createTransaction() in the wallet file.

--- src/XBlockchain/wallet.cpp
+++ src/XBlockchain/wallet.cpp
@@ -2,20 +2,21 @@
 // Created by youssef on 12/5/21.
 //
 #include "wallet.h"
+#include <algorithm>
 
 // Generate and store the private Key
 
-std::string generatePrivateKey()
+std::string generatePrivateKey() //MALO
 {
     return ""; //To implement using key
 }
 
-std::string getPrivateFromWallet()
+std::string getPrivateFromWallet() //MALO
 {
     return "";
 }
 
-std::string getPublicFromWallet()
+std::string getPublicFromWallet() //MALO
 {
     std::string privateKey = getPrivateFromWallet();
     std::string key; //add when implemented in transaction
@@ -72,14 +73,23 @@ std::pair<std::vector<UnspentTxOut>,int> findTxOutsForAmount(int amount, std::ve
     throw std::logic_error(errorMsg);
 }
 
-void createTxOuts(std::string receiverAddress, std::string myAddress, int amount, int leftOverAmount) {
+std::vector<TxOut> createTxOuts(std::string receiverAddress, std::string myAddress, int amount, int leftOverAmount) {
+    TxOut txOut1 = TxOut(receiverAddress, amount);
+    std::vector<TxOut> res;
+    res.push_back(txOut1);
+    if (leftOverAmount == 0) {
+        return res;
+    } else {
+        TxOut leftOverTx = TxOut(myAddress, leftOverAmount);
+        res.push_back(leftOverTx);
+        return res;
+    }
 //    TxOut txOut1 = new TxOut(receiverAddress, amount);
 //    if (leftOverAmount === 0) {
 //        return [txOut1]
 //    } else {
 //        TxOut leftOverTx = new TxOut(myAddress, leftOverAmount);
 //        return [txOut1, leftOverTx];
-    return;
 }
 
 std::vector<TxIn> getTxPoolIns(std::vector<Transaction> aTransactionPool) {
@@ -112,4 +122,48 @@ std::vector<UnspentTxOut> filterTxPoolTxs(std::vector<UnspentTxOut> unspentTxOut
         }
     }
     return newUnspentTxOuts;
+}
+
+TxIn toUnsignedTxIn(UnspentTxOut unspentTxOut) {
+    TxIn txIn = TxIn();
+    txIn.txOutId = unspentTxOut.txOutId;
+    txIn.txOutIndex = unspentTxOut.txOutIndex;
+    return txIn;
+}
+
+std::vector<TxIn> toUnsignedTxInArray(std::vector<UnspentTxOut> unspentTxOuts) {
+    std::vector<TxIn> res;
+    for (int i = 0; i < unspentTxOuts.size(); i++) {
+        res.push_back(toUnsignedTxIn(unspentTxOuts[i]));
+    };
+    return res;
+}
+
+
+Transaction createTransaction(std::string receiverAddress, int amount, std::string privateKey, std::vector<UnspentTxOut> unspentTxOuts, std::vector<Transaction> txPool)
+{
+    std::cout<< "txPool" << std::endl;
+    std::string myAddress = getPublicKey(privateKey);
+    std::vector<UnspentTxOut> myUnspentTxOutsA = findUnspentTxOuts(myAddress, unspentTxOuts);
+
+    std::vector<UnspentTxOut> myUnspentTxOuts = filterTxPoolTxs(myUnspentTxOutsA, txPool);
+
+    std::vector<UnspentTxOut> includedUnspentTxOuts;
+    int leftOverAmount;
+    std::tie(includedUnspentTxOuts, leftOverAmount) = findTxOutsForAmount(amount, myUnspentTxOuts);
+
+    std::vector<TxIn> unsignedTxIns = toUnsignedTxInArray(includedUnspentTxOuts);
+
+    Transaction tx;
+    tx.txIns = unsignedTxIns;
+    tx.txOuts = createTxOuts(receiverAddress, myAddress, amount, leftOverAmount);
+    tx.id = tx.getTransactionId();
+
+    //Signing the txIns
+    for(int i = 0; i < tx.txIns.size(); i++){
+        TxIn txIn = tx.txIns[i];
+        txIn.signature = tx.signTxIn(i, privateKey, unspentTxOuts);
+    }
+
+    return tx;
 }
