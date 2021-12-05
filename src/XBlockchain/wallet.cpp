78146706efae0cbd81d47f29b1ff44c5b7150ee8
youssef-chaabouni <youssef.chaabouni@polytechnique.edu>
2021-12-05 16:56:02 +0100

    Implemented findUnspentTxOuts(), getBalance(), findTxOutsForAmount() in the wallet.cpp file.

--- src/XBlockchain/wallet.cpp
+++ src/XBlockchain/wallet.cpp
@@ -34,14 +34,45 @@ void deleteWallet() {
     return;
 }
 
-
-
-std::string getBalance()
+std::vector<UnspentTxOut> findUnspentTxOuts(std::string ownerAddress, std::vector<UnspentTxOut> unspentTxOuts)
 {
-    return "";
+    std::vector<UnspentTxOut> res;
+    for (int i = 0; i < unspentTxOuts.size(); i++){
+        UnspentTxOut uTxO = unspentTxOuts[i];
+        if (uTxO.address == ownerAddress) {
+            res.push_back(uTxO);
+        };
+    }
+    return res;
 }
 
+int getBalance(std::string address, std::vector<UnspentTxOut> unspentTxOuts)
+{
+    std::vector<UnspentTxOut> ownedUnspentTxOuts = findUnspentTxOuts(address, unspentTxOuts);
+    int res;
+    for (int i = 0; i < ownedUnspentTxOuts.size(); i++){
+        UnspentTxOut uTxO = ownedUnspentTxOuts[i];
+        res += uTxO.amount;
+    };
+    return res;
+}
 
+std::pair<std::vector<UnspentTxOut>,int> findTxOutsForAmount(int amount, std::vector<UnspentTxOut> myUnspentTxOuts)
+{
+    int currentAmount = 0;
+    std::vector<UnspentTxOut> includedUnspentTxOuts;
+    for (int i = 0; i < myUnspentTxOuts.size(); i++){
+        UnspentTxOut myUnspentTxOut = myUnspentTxOuts[i];
+        includedUnspentTxOuts.push_back(myUnspentTxOut);
+        currentAmount += myUnspentTxOut.amount;
+        if (currentAmount >= amount){
+            int leftOverAmount = currentAmount - amount;
+            return std::make_pair(includedUnspentTxOuts, leftOverAmount);
+        }
+    }
+    std::string errorMsg = "Cannot create Transaction: insufficient total amount of unspent transaction outputs.";
+    throw std::logic_error(errorMsg);
+}
 
 void createTxOuts(std::string receiverAddress, std::string myAddress, int amount, int leftOverAmount) {
 //    TxOut txOut1 = new TxOut(receiverAddress, amount);
