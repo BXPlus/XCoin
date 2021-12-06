fb91d84b04758040cb3a0daedf464a6d188c3eb0
Malo-Lahogue <malo.lahogue@gmail.com>
2021-12-06 14:16:11 +0100

    Correction of unintetionally changed files

--- src/XNode/main.cpp
+++ src/XNode/main.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include "node.h"
-#include "keys.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,7 +15,7 @@ int main(int argc, char *argv[])
                          "    /\\_\\\\ \\_\\\\ \\____/ \\ \\_____\\ /\\_____\\\\ \\_\\ \\_\\\n"
                          "    \\/_/ \\/_/ \\/___/   \\/_____/ \\/_____/ \\/_/\\/_/";
     std::cout << banner << std::endl;
-    /*
+
     std::cout << "                                  Blockchain node    \n" << std::endl;
     std::vector<std::string> DNSS = std::vector<std::string>();
     for(int i = 1; i < argc; i++ )
@@ -30,17 +29,7 @@ int main(int argc, char *argv[])
             std::cout << "Node will terminate..." << std::endl;
         break;
     }
-    */
-    Keys key =  Keys();
-    //std::cout << key.getPriv() << std::endl;
-   //std::cout << key.getPub() << std::endl;
 
-    std::string priv = "3DAE35FDCAA5338F6CDD1E13AC2400B654CCADA13A7F8133D079E26A1CC0D202";
-    std::string pub = "04923B77634E4227516240612F729286FB44FFBF1FE6954628DCCDCB9962CB42825ADD808F5A4781BC487B592AB8DD6F31773678D5FA064483B63D673A254862B5";
-    std::pair<uint8_t*, uint32_t> sgn = sign(priv, "message");
-
-
-    std::cout << (typeid(sgn) == typeid(std::pair<uint8_t, uint32_t>)) << "  ,  " << sgn.second << std::endl;
 
 
 
