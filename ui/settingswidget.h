57087a170f7ee39bf59eed5d695adcb652635d9b
T1m0uss3 <62285023+tim-vlc@users.noreply.github.com>
2021-12-13 23:16:48 +0100

    Finished up the Settings Page

--- ui/settingswidget.h
+++ ui/settingswidget.h
@@ -6,6 +6,7 @@
 #include "mainwindow.h"
 #include <QComboBox>
 #include <QScrollArea>
+#include <QLineEdit>
 
 class SettingsWidget : public QWidget
 {
@@ -16,21 +17,30 @@ public:
     QWidget* mainWindow;
 private:
     QWidget* mainWidget;
+    QWidget* topBox;
+    QWidget* emptyWidget;
 
     QVBoxLayout* mainLayout;
-    QWidget* emptyWidget;
+    QHBoxLayout* topLayout;
+    QLabel* title;
 
     QList<QHBoxLayout*> hList;
     QList<QLabel*> lList = {};
     QList<QLabel*> sList = {};
     QList<QAbstractButton*> bList= {};
     QList<QString> stringList;
+    QList<QWidget*> widgetList;
+    QList<QLineEdit*> lineList;
+
+    QPushButton* editBtn;
 
     int flNode_count = 0;
     int appearance_count = 0;
+    int counter = 0;
 private slots:
     void flnode_state();
     void appearance_state();
+    void edit_press();
 };
 
 #endif // SETTINGSWIDGET_H
