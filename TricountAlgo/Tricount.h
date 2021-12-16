075bfc4cdcbcba33d26bc7ff4268b1c2c3d5b075
jeansebastien-gaultier <91254530+jeansebastien-gaultier@users.noreply.github.com>
2021-12-16 11:13:14 +0100

    Update Tricount.h
    Start of the "Tricount" implementation (all commented in case this is wrong)

--- TricountAlgo/Tricount.h
+++ TricountAlgo/Tricount.h
@@ -6,10 +6,34 @@
 #define XBLOCKCHAIN_TRICOUNT_H
 
 #include <iostream>
+#include <string>
+#include <list>
 
 class Tricount {
 
 };
+
+
+//This class will deal the transactions between friends, who owes who money
+//This for now is a basic idea implementation
+
+//class Tricount {
+//public:
+//    double total_spending;
+//    std::list<char> users;
+
+//    Tricount(){
+//        total_spending=0;
+//    }
+//    ~Tricount(){
+//        delete total_spending;
+//    }
+//    double get_tot_spending();  //See how much money was spent
+//    void add_user(char user_name);            //Add users to the tricount
+//    void add_expense(double money, char user1, char user2);         //Add expense to the total_spending
+//    void add_money_transfer(double money, char user1, char user2);  //Somebody pays back what they owe
+
+//};
 
 
 #endif //XBLOCKCHAIN_TRICOUNT_H
